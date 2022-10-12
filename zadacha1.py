44b1debf667e85b2f5fdf59a2ef085184c3e3e0d
Katya Kundysheva <63952507+KatyaKundysheva@users.noreply.github.com>
2022-10-12 21:54:39 +0300

    Add files via upload

---
+++ zadacha1.py
@@ -0,0 +1,8 @@
+﻿import math
+try:
+    n = int(input())
+except ValueError:
+    print("Неверный ввод")
+print(math.sin(math.radians(n)))
+print(math.cos(math.radians(n)))
+print(math.tan(math.radians(n)))
